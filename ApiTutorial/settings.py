174d68f9fa5b529407a13bc17a010ab8c83a2bc1
MUNSHI AMINUR RASHID <aminurrashid224@gmail.com>
2023-08-16 17:01:42 +0530

    Add files via upload

--- ApiTutorial/settings.py
+++ ApiTutorial/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)!x!^&czj-p^li_o&zgh3v6=gy#69bq)b%wz!(@syjec(4*j(g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['AminurRashid.pythonanywhere.com']
 
 
 # Application definition
